56483972aa210b70224d5d96c860ff47a05b5ceb
Jay <7327430+jay-ramani@users.noreply.github.com>
2023-12-04 02:03:06 -0500

    Update Video Metadata DB Build.cmd
    Update path

--- Video Metadata DB Build.cmd
+++ Video Metadata DB Build.cmd
@@ -3,9 +3,9 @@ cls
 set PATH=%PATH%;"C:\Program Files\Python"
 :loop_grab_metadata
 IF %1=="" GOTO completed
-python "G:\My Drive\Projects\Video Resolution\video_metadata_db.py" --percentage-completion %1
+python "G:\My Drive\Projects\Video Metadata DB\video_metadata_db.py" --percentage-completion %1
 SHIFT
 GOTO loop_grab_metadata
 :completed
-sort /R "G:\My Drive\Projects\Video Resolution\video_metadata_db.txt" /O "G:\My Drive\Projects\Video Resolution\video_metadata_db.txt"
+sort /R "G:\My Drive\Projects\Video Metadata DB\video_metadata_db.txt" /O "G:\My Drive\Projects\Video Metadata DB\video_metadata_db.txt"
 pause
