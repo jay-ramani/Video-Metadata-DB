3da0c76b1302b53b27d637c9913d7d77b5d9ed1e
Jay <7327430+jay-ramani@users.noreply.github.com>
2023-12-04 01:04:40 -0500

    Add files via upload
    DOS shell command helper to (differentially) update the metadata CSV database

---
+++ Video Metadata DB Update.cmd
@@ -0,0 +1,11 @@
+@echo off
+cls
+set PATH=%PATH%;"C:\Program Files\Python"
+:loop_grab_resolutions
+IF %1=="" GOTO completed
+python "G:\My Drive\Projects\Video Resolution\video_resolution.py" --percentage-completion --update-metadata-db %1
+SHIFT
+GOTO loop_grab_resolutions
+:completed
+sort /R "G:\My Drive\Projects\Video Resolution\video_resolution.txt" /O "G:\My Drive\Projects\Video Resolution\video_resolution.txt"
+pause
