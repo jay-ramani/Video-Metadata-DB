6c4b1f17c943aea6ea81850bfcfaf82bb8adc2de
Jay <7327430+jay-ramani@users.noreply.github.com>
2024-10-05 07:21:22 -0400

    Update Video Metadata DB Merge and Sort.cmd

--- Video Metadata DB Merge and Sort.cmd
+++ Video Metadata DB Merge and Sort.cmd
@@ -2,5 +2,5 @@
 cls
 set PATH=%PATH%;"C:\Program Files\Python"
 echo %*
-python "G:\My Drive\Projects\Video Metadata DB\video_metadata_db.py" --merge-metadata %*
+python "G:\My Drive\Projects\Video Metadata DB\video_metadata_db.py" --merge-metadata --verbose %*
 pause
